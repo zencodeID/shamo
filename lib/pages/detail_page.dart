0d5bf298da802a58abc11191fbffa377243c4316
zencode <zencode.id@gmail.com>
2022-10-14 08:59:54 +0700

    sudah fix

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
+import 'package:shamo/widgets/facility_item.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({super.key});
@@ -55,23 +56,134 @@ class DetailPage extends StatelessWidget {
                     color: whiteColor,
                   ),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       SizedBox(
-                        height: 10,
+                        height: 30,
                       ),
                       Padding(
                         padding: EdgeInsets.symmetric(
                           horizontal: edge,
                         ),
-                        child: Column(
-                          children: [],
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'kurateka Spoot',
+                                  style: blackTextStyle.copyWith(
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Text.rich(
+                                  TextSpan(
+                                    text: '\$52',
+                                    style: purpleTextstyle.copyWith(
+                                      fontSize: 16,
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                        text: '/month',
+                                        style: greyTextStyle.copyWith(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                  color: greyColor,
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                )
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                //NOTE: Main Facilities //
+                Padding(
+                  padding: EdgeInsets.only(left: edge),
+                  child: Text(
+                    'Main Facilities',
+                    style: regularTextstyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: edge,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      FacilityItem(
+                        name: 'Kitchen',
+                        total: 2,
+                        imageUrl: 'assets/icon_kitchen.png',
+                      ),
+                      FacilityItem(
+                        name: "Bedroom",
+                        total: 3,
+                        imageUrl: 'assets/icon_bedroom.png',
+                      ),
+                      FacilityItem(
+                        name: 'Big Lemari',
+                        total: 2,
+                        imageUrl: 'assets/icon_cupboard.png',
+                      ),
+                    ],
+                  ),
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
